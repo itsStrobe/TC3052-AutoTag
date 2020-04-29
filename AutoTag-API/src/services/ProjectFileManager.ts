59da6fabaa74624db69313970f174c3d2729f124
strobe <josezavala0297@gmail.com>
2020-04-29 10:05:29 -0500

    Added Enum to Text conversions

--- AutoTag-API/src/services/ProjectFileManager.ts
+++ AutoTag-API/src/services/ProjectFileManager.ts
@@ -1,5 +1,7 @@
 import { Container, Service, Inject } from 'typedi';
 import AWSAccessorService from './AWSFileAccesor';
+import { Project } from '../model/project';
+import { User } from '../model/user';
 
 enum ProjectType {
   SentimentAnalysis = 0,
@@ -27,6 +29,127 @@ export default class ProjectFileManagerService {
   @Inject()
   awsClient : AWSAccessorService; // AWS File Accessor Service
 
+  // == Helper Methods ==
+  /**
+   * ProjectTypeToText
+   */
+  public ProjectTypeToText(projectType : ProjectType) : string {
+    switch (projectType) {
+      case ProjectType.SentimentAnalysis:
+        return 'SentimentAnalysis';
+
+      case ProjectType.DocumentClassification:
+        return 'DocumentClassification';
+      
+      case ProjectType.POSTagging:
+        return 'POSTagging';
+
+      case ProjectType.NERTagging:
+        return 'NERTagging';
+    
+      default:
+        return null;
+    }
+  }
+
+  /**
+   * TextToProjectType
+   */
+  public TextToProjectType(projectType : string) : ProjectType {
+    switch (projectType) {
+      case 'SentimentAnalysis':
+        return ProjectType.SentimentAnalysis;
+
+        case 'DocumentClassification':
+          return ProjectType.DocumentClassification;
+        
+        case 'POSTagging':
+          return ProjectType.POSTagging;
+  
+        case 'NERTagging':
+          return ProjectType.NERTagging;
+      
+        default:
+          return null;
+    }
+  }
+  
+  /**
+   * DataFormatToText
+   */
+  public DataFormatToText(dataFormat : DataFormat) : string {
+    switch (dataFormat) {
+      case DataFormat.CSV:
+        return 'CSV';
+
+      case DataFormat.TXT:
+        return 'TXT';
+    
+      default:
+        return null;
+    }
+  }
+
+  /**
+   * TextToDataFormat
+   */
+  public TextToDataFormat(dataFormat : string) : DataFormat {
+    switch (dataFormat) {
+      case 'CSV':
+        return DataFormat.CSV;
+
+      case 'TXT':
+        return DataFormat.TXT;
+    
+      default:
+        return null;
+    }
+  }
+
+  /**
+   * StatusToText
+   */
+  public StatusToText(status : Status) : string {
+    switch (status) {
+      case Status.NotTagged:
+        return 'NotTagged';
+
+      case Status.InProgress:
+        return 'InProgress';
+
+      case Status.PreTagged:
+        return 'PreTagged';
+
+      case Status.Tagged:
+        return 'Tagged';
+    
+      default:
+        return null;
+    }
+  }
+
+  /**
+   * TextToStatus
+   */
+  public TextToStatus(status : string) : Status {
+    switch (status) {
+      case 'NotTagged':
+        return Status.NotTagged;
+
+      case 'InProgress':
+        return Status.InProgress;
+
+      case 'PreTagged':
+        return Status.PreTagged;
+
+      case 'Tagged':
+        return Status.Tagged;
+    
+      default:
+        return null;
+    }
+  }
+
   // TODO: Initialize CSV Project
   /*
       Based on a Project Object (with ProjectType=CSV), creates all necessary
@@ -44,13 +167,26 @@ export default class ProjectFileManagerService {
       Based on a Generic Project Object, creates all necessary
       Files in AWS
   */
-  
-  // TODO: Initialize Project
-  /*
-      Create a Project Object based on Parameters.
-  
-      Returns Project Object
-  */
+
+  /**
+   * InitializeProject
+   * 
+   * Create a Project Object based on Parameters.
+   * Returns Project Object
+   */
+  public InitializeProject(name : string, owner : User, description : string, projectType : string, dataFormat : string, tags : string[]) : Project {
+    let project = new Project();
+
+    project.name = name;
+    project.owner = owner;
+    project.description = description;
+    project.type = this.TextToProjectType(projectType);
+    project.projectDataFormat = dataFormat; // TODO: Project.projectDataFormat must be a number
+    // TODO: Project Tags
+    project.status = Status.NotTagged;
+
+    return project;
+  }
   
   // TODO: Set Data Files to Project
   
